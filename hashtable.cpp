ef650319e1854bc03032cb010fb8aa526815bb3a
marsnebulasoup <marsnebulasoup@gmail.com>
2022-10-25 16:00:28 -0500

    Stubbed out hashtable

--- hashtable.cpp
+++ hashtable.cpp
@@ -3,3 +3,42 @@ Name: Kevin Bryniak
 Assignment: Hash Tables
 A example implementation of a hash table.
 **********************/
+
+#include "hashtable.h"
+
+HashTable::HashTable()
+{
+}
+
+HashTable::~HashTable()
+{
+}
+
+int hash(int)
+{
+  return 0;
+}
+
+bool HashTable::insertEntry(int id, string *data)
+{
+  return true;
+}
+
+bool HashTable::removeEntry(int id)
+{
+  return true;
+}
+
+bool HashTable::getData(int id, Data *to)
+{
+  return true;
+}
+
+int HashTable::getCount()
+{
+  return 0;
+}
+
+void HashTable::printTable()
+{
+}
--- hashtable.h
+++ hashtable.h
@@ -3,3 +3,34 @@ Name: Kevin Bryniak
 Assignment: Hash Tables
 A example implementation of a hash table.
 **********************/
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include "data.h"
+#include "linkedlist.h"
+#include "string"
+
+using std::string;
+
+#define SIZE 15
+
+class HashTable
+{
+private:
+  int count;
+  LinkedList table[SIZE];
+  int hash(int);
+
+public:
+  HashTable();
+  ~HashTable();
+
+  bool insertEntry(int, string *);
+  bool removeEntry(int);
+  bool getData(int, Data *);
+  int getCount();
+  void printTable();
+};
+
+#endif
